cdc0fc0ebd2cd0c10a1e88d158a83566b0d44c6e
adabs-urdum <cyrill.lehmann@clus.ch>
2019-07-07 17:36:32 +0200

    added materialswitch

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,226 +18,102 @@ function WebGLThreeJS(){
       shadowHelper,
       floatingTotal,
       people,
-      parts;
+      closestRotation,
+      currentRotation,
+      parts,
+      currentBackside,
+      path,
+      currentPerson;
 
   const THREE = require('three');
-  const OBJLoader = require('three-obj-loader');
-  OBJLoader(THREE);
-  const MTLLoader = require('three-mtl-loader');
 
   function init(){
-    console.log('init');
+
     setVars();
     bindEvents();
     addParts();
-    addHair();
-    addEyes();
-    addMouth();
-    addTorso();
     mainLoop();
-  }
 
-  function addParts(){
-    
-  }
-
-  function addHair(){
-    let cubeGeometry = new THREE.BoxGeometry(1,0.2192982456,1);
-    let loader = new THREE.TextureLoader();
-    let materialArray = [
-        new THREE.MeshLambertMaterial({
-          flatShading: false,
-          color: 0xFFFFFF,
-          map: loader.load('/dist/img/team/yanik_top.jpg'),
-        }),
-        new THREE.MeshLambertMaterial({
-          flatShading: false,
-          color: 0xFFFFFF,
-          map: loader.load('/dist/img/team/damian_top.jpg'),
-        }),
-        new THREE.MeshLambertMaterial({
-          flatShading: false,
-          color: 0x333333,
-        }),
-        new THREE.MeshLambertMaterial({
-          flatShading: false,
-          color: 0x333333,
-        }),
-        new THREE.MeshLambertMaterial({
-          flatShading: false,
-          color: 0xFFFFFF,
-          map: loader.load('/dist/img/team/sarah_top.jpg'),
-        }),
-        new THREE.MeshLambertMaterial({
-          flatShading: false,
-          color: 0xFFFFFF,
-          map: loader.load('/dist/img/team/laurin_top.jpg'),
-        }),
-    ];
-    let mesh = new THREE.Mesh( cubeGeometry, materialArray );
-
-    mesh.geometry.dynamic = true;
-    mesh.geometry.computeVertexNormals();
-
-    mesh.material.needsUpdate = true;
-
-    // mesh.receiveShadow = true;
-    // mesh.castShadow = true;
-
-    mesh.position.y += 0.135;
-
-    clickables.push(mesh);
-    scene.add(mesh);
-  }
-
-  function addEyes(){
-    let cubeGeometry = new THREE.BoxGeometry(1,0.09868421053,1);
-    let loader = new THREE.TextureLoader();
-    let materialArray = [
-      new THREE.MeshLambertMaterial({
-        flatShading: false,
-        color: 0xFFFFFF,
-        map: loader.load('/dist/img/team/yanik_eyes.jpg'),
-      }),
-      new THREE.MeshLambertMaterial({
-        flatShading: false,
-        color: 0xFFFFFF,
-        map: loader.load('/dist/img/team/damian_eyes.jpg'),
-      }),
-      new THREE.MeshLambertMaterial({
-        flatShading: false,
-        color: 0x333333,
-      }),
-      new THREE.MeshLambertMaterial({
-        flatShading: false,
-        color: 0x333333,
-      }),
-      new THREE.MeshLambertMaterial({
-        flatShading: false,
-        color: 0xFFFFFF,
-        map: loader.load('/dist/img/team/sarah_eyes.jpg'),
-      }),
-      new THREE.MeshLambertMaterial({
-        flatShading: false,
-        color: 0xFFFFFF,
-        map: loader.load('/dist/img/team/laurin_eyes.jpg'),
-      }),
-    ];
-    let mesh = new THREE.Mesh( cubeGeometry, materialArray );
-
-    mesh.geometry.dynamic = true;
-    mesh.geometry.computeVertexNormals();
-
-    mesh.material.needsUpdate = true;
-
-    // mesh.receiveShadow = true;
-    // mesh.castShadow = true;
-
-    mesh.position.y -= 0.035;
-
-    clickables.push(mesh);
-    scene.add(mesh);
-  }
-
-  function addMouth(){
-    let cubeGeometry = new THREE.BoxGeometry(1,0.1129385965,1);
-    let loader = new THREE.TextureLoader();
-    let materialArray = [
-      new THREE.MeshLambertMaterial({
-        flatShading: false,
-        color: 0xFFFFFF,
-        map: loader.load('/dist/img/team/yanik_mouth.jpg'),
-      }),
-      new THREE.MeshLambertMaterial({
-        flatShading: false,
-        color: 0xFFFFFF,
-        map: loader.load('/dist/img/team/damian_mouth.jpg'),
-      }),
-      new THREE.MeshLambertMaterial({
-        flatShading: false,
-        color: 0x333333,
-      }),
-      new THREE.MeshLambertMaterial({
-        flatShading: false,
-        color: 0x333333,
-      }),
-      new THREE.MeshLambertMaterial({
-        flatShading: false,
-        color: 0xFFFFFF,
-        map: loader.load('/dist/img/team/sarah_mouth.jpg'),
-      }),
-      new THREE.MeshLambertMaterial({
-        flatShading: false,
-        color: 0xFFFFFF,
-        map: loader.load('/dist/img/team/laurin_mouth.jpg'),
-      }),
-    ];
-    let mesh = new THREE.Mesh( cubeGeometry, materialArray );
-
-    mesh.geometry.dynamic = true;
-    mesh.geometry.computeVertexNormals();
-
-    mesh.material.needsUpdate = true;
-
-    // mesh.receiveShadow = true;
-    // mesh.castShadow = true;
-
-    mesh.position.y -= 0.15;
-
-    clickables.push(mesh);
-    scene.add(mesh);
-  }
-
-  function addTorso(){
-    let cubeGeometry = new THREE.BoxGeometry(1,0.5679824561,1);
-    let loader = new THREE.TextureLoader();
-    let materialArray = [
-      new THREE.MeshLambertMaterial({
-        flatShading: false,
-        color: 0xFFFFFF,
-        map: loader.load('/dist/img/team/yanik_torso.jpg'),
-      }),
-      new THREE.MeshLambertMaterial({
-        flatShading: false,
-        color: 0xFFFFFF,
-        map: loader.load('/dist/img/team/damian_torso.jpg'),
-      }),
-      new THREE.MeshLambertMaterial({
-        flatShading: false,
-        color: 0x333333,
-      }),
-      new THREE.MeshLambertMaterial({
-        flatShading: false,
-        color: 0x333333,
-      }),
-      new THREE.MeshLambertMaterial({
-        flatShading: false,
-        color: 0xFFFFFF,
-        map: loader.load('/dist/img/team/sarah_torso.jpg'),
-      }),
-      new THREE.MeshLambertMaterial({
-        flatShading: false,
-        color: 0xFFFFFF,
-        map: loader.load('/dist/img/team/laurin_torso.jpg'),
-      }),
-    ];
-    let mesh = new THREE.Mesh( cubeGeometry, materialArray );
-
-    mesh.geometry.dynamic = true;
-    mesh.geometry.computeVertexNormals();
-
-    mesh.material.needsUpdate = true;
-
-    // mesh.receiveShadow = true;
-    // mesh.castShadow = true;
-
-    mesh.position.y -= 0.5;
-
-    clickables.push(mesh);
-    scene.add(mesh);
   }
 
   function setVars(){
+
+    currentRotation = 0;
+    closestRotation = 0;
+    closestRotationDegree = closestRotation * 180 / Math.PI;
+    floatingTotal = 0;
+    floatingDirection = true;
+
+    rotationPoints = [
+      0,
+      Math.PI / 2, // 90deg
+      Math.PI, // 180deg
+      Math.PI * 1.5, // 270deg
+      Math.PI * 2, // 360deg
+      Math.PI / -2, // -90deg
+      Math.PI * -1, // -180deg
+      Math.PI * -1.5, // -270deg
+      Math.PI * -2 // -360deg
+    ];
+    runEasing = true;
+
+    currentPerson = 4;
+
+    people = [
+      {
+        name: 'cyrill',
+      },
+      {
+        name: 'damian',
+      },
+      {
+        name: 'kajo',
+      },
+      {
+        name: 'kevin',
+      },
+      {
+        name: 'laurin',
+      },
+      {
+        name: 'oli',
+      },
+      {
+        name: 'romina',
+      },
+      {
+        name: 'sarah',
+      },
+      {
+        name: 'yanik',
+      }
+    ];
+
+    path = '/dist/img/team/';
+
+    parts = [
+      {
+        name: 'top',
+        height: 0.2192982456,
+        posY: 0.135
+      },
+      {
+        name: 'eyes',
+        height: 0.09868421053,
+        posY: -0.035,
+      },
+      {
+        name: 'mouth',
+        height: 0.1129385965,
+        posY: -0.15,
+      },
+      {
+        name: 'torso',
+        height: 0.5679824561,
+        posY: -0.5,
+      }
+    ];
+
     scene = new THREE.Scene();
 
     camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 1, 250);
@@ -249,6 +125,35 @@ function WebGLThreeJS(){
     mouse.x = mouse.y = -1;
     clickables = [];
 
+    setLights();
+
+    domContainer = document.getElementsByClassName('rubiks')[0];
+
+    setRenderer();
+
+    domContainer.appendChild(renderer.domElement);
+
+  }
+
+  function setRenderer(){
+    renderer = new THREE.WebGLRenderer({
+      alpha: true,
+      antialias: true,
+    });
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMapSoft = true;
+    renderer.shadowCameraNear = 3;
+    renderer.shadowCameraFar = camera.far;
+    renderer.shadowCameraFov = 50;
+
+    renderer.shadowMapBias = 0.0039;
+    renderer.shadowMapDarkness = 0.5;
+    renderer.shadowMapWidth = 1024;
+    renderer.shadowMapHeight = 1024;
+    renderer.setSize(domContainer.offsetWidth, domContainer.offsetHeight);
+  }
+
+  function setLights(){
     // HemisphereLight( skyColor : Integer, groundColor : Integer, intensity : Float )
     hemisphereLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.8);
     scene.add(hemisphereLight);
@@ -277,48 +182,78 @@ function WebGLThreeJS(){
     spotLight.position.set(0,0.3,3);
     // spotLight.castShadow = true;
     // scene.add(spotLight);
+  }
 
-    domContainer = document.getElementsByClassName('rubiks')[0];
+  function addParts(){
 
-    renderer = new THREE.WebGLRenderer({
-      alpha: true,
-      antialias: true,
+    parts.map(part => {
+
+      let cubeGeometry = new THREE.BoxGeometry(1,part.height,1);
+      let loader = new THREE.TextureLoader();
+
+      let materials = [
+        new THREE.MeshLambertMaterial({
+          flatShading: false,
+          color: 0x222222,
+        }),
+      ];
+      materials = materials.concat(people.map(person => {
+        return new THREE.MeshLambertMaterial({
+          flatShading: false,
+          color: 0xffffff,
+          map: loader.load(path + person.name + '_' + part.name + '.jpg'),
+        })
+      }));
+
+      cubeGeometry.materials = materials;
+
+      cubeGeometry.computeFaceNormals();
+      cubeGeometry.computeVertexNormals();
+
+      let mesh = new THREE.Mesh( cubeGeometry, materials );
+
+      // right
+      mesh.geometry.faces[0].materialIndex = 1;
+      mesh.geometry.faces[1].materialIndex = 1;
+
+      // left
+      mesh.geometry.faces[2].materialIndex = 2;
+      mesh.geometry.faces[3].materialIndex = 2;
+
+      // up
+      mesh.geometry.faces[4].materialIndex = 0;
+      mesh.geometry.faces[5].materialIndex = 0;
+
+      // down
+      mesh.geometry.faces[6].materialIndex = 0;
+      mesh.geometry.faces[7].materialIndex = 0;
+
+      // front
+      mesh.geometry.faces[8].materialIndex = 3;
+      mesh.geometry.faces[9].materialIndex = 3;
+
+      // back
+      mesh.geometry.faces[10].materialIndex = 4;
+      mesh.geometry.faces[11].materialIndex = 4;
+
+      mesh.geometry.groupsNeedUpdate = true;
+
+      mesh.geometry.dynamic = true;
+      mesh.geometry.computeVertexNormals();
+
+      currentMaterialIndex = 4;
+
+      mesh.material.needsUpdate = true;
+
+      // mesh.receiveShadow = true;
+      // mesh.castShadow = true;
+
+      mesh.position.y += part.posY;
+
+      currentTarget = mesh;
+      clickables.push(mesh);
+      scene.add(mesh);
     });
-    renderer.shadowMap.enabled = true;
-    renderer.shadowMapSoft = true;
-    renderer.shadowCameraNear = 3;
-    renderer.shadowCameraFar = camera.far;
-    renderer.shadowCameraFov = 50;
-
-    renderer.shadowMapBias = 0.0039;
-    renderer.shadowMapDarkness = 0.5;
-    renderer.shadowMapWidth = 1024;
-    renderer.shadowMapHeight = 1024;
-    renderer.setSize(domContainer.offsetWidth, domContainer.offsetHeight);
-
-    domContainer.appendChild(renderer.domElement);
-
-    floatingTotal = 0;
-    floatingDirection = true;
-
-    people = [
-      'cyrill',
-      'damian',
-      'kajo',
-      'kevin',
-      'laurin',
-      'oli',
-      'romina',
-      'sarah',
-      'yanik'
-    ];
-
-    parts = [
-      'eyes',
-      'mouth',
-      'top',
-      'torso'
-    ];
 
   }
 
@@ -331,6 +266,7 @@ function WebGLThreeJS(){
 
   function onMouseDown(e){
 
+    runEasing = false;
     xClicked = e.clientX;
     yClicked = e.clientY;
 
@@ -344,12 +280,16 @@ function WebGLThreeJS(){
 
     if(intersects.length){
       currentTarget = intersects[0].object;
+      resetCurrentRotation();
+      setClosest();
       domContainer.addEventListener('mousemove', onMouseMove);
     }
 
   }
 
   function onTouchDown(e){
+
+    runEasing = false;
 
     xClicked = e.touches[0].clientX;
     yClicked = e.touches[0].clientY;
@@ -364,6 +304,8 @@ function WebGLThreeJS(){
 
     if(intersects.length){
       currentTarget = intersects[0].object;
+      resetCurrentRotation();
+      setClosest();
       domContainer.addEventListener('touchmove', onTouchMove);
     }
 
@@ -373,7 +315,6 @@ function WebGLThreeJS(){
     const xMouse = e.clientX;
     const yMouse = e.clientY;
     currentTarget.rotation.y += (xMouse - xClicked) / 160;
-    renderer.shadowMap.needsUpdate = true;
     xClicked = xMouse;
     yClicked = yMouse;
   }
@@ -389,10 +330,82 @@ function WebGLThreeJS(){
 
   function onMouseUp(){
     domContainer.removeEventListener('mousemove', onMouseMove);
+    runEasing = true;
+    resetCurrentRotation();
+    setClosest();
   }
 
   function onTouchUp(){
     domContainer.removeEventListener('touchmove', onTouchMove);
+    runEasing = true;
+    resetCurrentRotation();
+    setClosest();
+  }
+
+  function setNewFace(){
+    currentTarget.geometry.groupsNeedUpdate = true;
+    let turn = closestRotationDegree / 90 < 0 ? closestRotationDegree / -90 : closestRotationDegree / 90;
+    if(turn == 0){turn = 1}
+
+    currentMaterialIndex += 1;
+    if(currentMaterialIndex > currentTarget.material.length - 1){
+      currentMaterialIndex = 1;
+    }
+
+    const steps = {
+      1: [10,11],
+      2: [8,9],
+      3: [2,3],
+      4: [0,1]
+    };
+
+    const faceKeys = steps[turn];
+
+    faceKeys.map(faceKey => {
+      currentTarget.geometry.faces[faceKey].materialIndex = currentMaterialIndex;
+    });
+    currentTarget.geometry.groupsNeedUpdate = true;
+
+  }
+
+  function setClosest(){
+    currentRotation = (currentTarget.rotation.y);
+    closestRotation = rotationPoints.reduce(function(prev, curr) {
+      return (Math.abs(curr - currentRotation) < Math.abs(prev - currentRotation) ? curr : prev);
+    });
+    closestRotationDegree = closestRotation * 180 / Math.PI;
+  }
+
+  function fitCurrent(){
+    const difference = (closestRotation - currentRotation);
+    if(difference >= 0.008 || difference <= -0.008){
+      if(difference > 1 || difference < -0.1){
+        currentTarget.rotation.y += difference * 0.1;
+      }
+      else if(difference > 0.01 || difference < -0.01){
+        currentTarget.rotation.y += difference * 0.071;
+      }
+      else if(difference <= 0.01 || difference >= -0.01){
+        currentTarget.rotation.y += difference * 0.061;
+      }
+    }
+    else{
+      runEasing = false;
+      setNewFace();
+    }
+    currentRotation = (currentTarget.rotation.y);
+  }
+
+  function resetCurrentRotation(){
+    currentRotation = currentTarget.rotation.y;
+    if(currentRotation <= (Math.PI * -2)){
+      currentTarget.rotation.y = currentRotation + (Math.PI * 2);
+      currentRotation = (currentTarget.rotation.y);
+    }
+    else if(currentRotation >= (Math.PI * 2)){
+      currentTarget.rotation.y = currentRotation + (Math.PI * -2);
+      currentRotation = (currentTarget.rotation.y);
+    }
   }
 
   function resizeRenderer(){
@@ -425,6 +438,9 @@ function WebGLThreeJS(){
 
   function mainLoop(){
     renderer.render(scene, camera);
+    if(runEasing){
+      fitCurrent();
+    }
     // float();
     requestAnimationFrame(mainLoop);
   }
